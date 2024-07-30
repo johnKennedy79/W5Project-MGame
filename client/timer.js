50363c5809583fb80b5e97f580f3851ec209b16b
subhadra30 <k.subhadra@gmail.com>
2024-07-30 07:51:58 +0100

    Timer added in footer

---
+++ client/timer.js
@@ -0,0 +1,44 @@
+const startButton = document.getElementById("startBtn");
+const stopButton = document.getElementById("stopBtn");
+const restartButton = document.getElementById("restartBtn");
+
+const counterDisplayMin = document.getElementById("counterDisplayMin");
+const counterDisplaySec = document.getElementById("counterDisplaySec");
+
+let stopWatchSec = 0;
+let stopWatchMin = 1;
+
+let counterMinuteInterval;
+let counterSecondInterval;
+counterDisplayMin.textContent = "00";
+counterDisplaySec.textContent = "00";
+
+startButton.addEventListener("click", function () {
+  counterSecondInterval = setInterval(function () {
+    if (stopWatchSec < 9) {
+      counterDisplaySec.textContent = "0" + ++stopWatchSec;
+    } else {
+      counterDisplaySec.textContent = ++stopWatchSec;
+    }
+    if (stopWatchSec == 60) {
+      stopWatchSec = 0;
+      if (stopWatchMin < 10) {
+        counterDisplayMin.textContent = "0" + stopWatchMin++;
+      } else {
+        counterDisplayMin.textContent = stopWatchMin++;
+      }
+    }
+    console.log(stopWatchSec);
+  }, 1000);
+});
+
+restartButton.addEventListener("click", function () {
+  counterDisplayMin.textContent = "00";
+  counterDisplaySec.textContent = "00";
+  stopWatchMin = 0;
+  stopWatchSec = 0;
+  clearInterval(counterSecondInterval);
+});
+stopButton.addEventListener("click", function () {
+  clearInterval(counterSecondInterval);
+});
