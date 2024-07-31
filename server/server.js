899abc07767bcaf5cc03421ad753a3254c299ba4
John Kennedy <john,m,r,kennedy.jk@gmail.com>
2024-07-31 12:31:27 +0100

    bugfix to card flip plus get request for cards and client js merged to main js file

--- server/server.js
+++ server/server.js
@@ -11,12 +11,30 @@ app.get("/", (req, res) => {
   res.send("server root route");
 });
 
+// get card data from data base
+app.get("/cards", async function (req, res) {
+  const result = await db.query(`SELECT * FROM cards`);
+  const cardrow = result.rows;
+  res.json(cardrow);
+});
+
+// get sorted data from leader board database
 app.get("/leaderboard", async function (req, res) {
   const lbresults = await db.query(
     `SELECT * FROM LEADERBOARD ORDER BY timemin ASC, timesec ASC LIMIT 3`
   );
   const lbTopResults = lbresults.rows;
   res.json(lbTopResults);
+});
+
+// post new scores to leaderboard database
+app.post("/leaderboard", (req, res) => {
+  console.log(req.body);
+  res.json("time saved");
+  db.query(
+    `INSERT INTO leaderboard (userName, timeMin, timeSec) VALUES ($1, $2, $3)`,
+    [req.body.userName, req.body.timeMin, req.body.timeSec]
+  );
 });
 
 app.listen("8080", () => {
