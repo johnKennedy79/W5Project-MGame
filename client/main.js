eb28f6a4bb986ac385db15b0845e32a62bf1be4d
James Withnall <jameswithnall@icloud.com>
2024-07-31 09:29:52 +0100

    ScoresUpdate

--- client/main.js
+++ client/main.js
@@ -5,27 +5,37 @@ let toggleSideBar = function () {
   let getSideBar = document.querySelector(".sideBar");
 
   if (toggleSideBarStatus === false) {
-    getSideBar.style.visibility = "visible";
+    getSideBar.style.translate = "0%";
     toggleSideBarStatus = true;
   } else if (toggleSideBarStatus === true) {
-    getSideBar.style.visibility = "hidden";
+    getSideBar.style.translate = "-100%";
     toggleSideBarStatus = false;
   }
 };
+
 scoreBtn.addEventListener("click", toggleSideBar);
 
-const updateBestScores = (newTime) => {
-  const bestScores = [
-    parseInt(document.getElementById("ps1").innerText),
-    parseInt(document.getElementById("ps2").innerText),
-    parseInt(document.getElementById("ps3").innerText),
-  ];
+async function leaderboardscores() {
+  const lbresults = await fetch("http://localhost:8080/leaderboard");
+  const lbdata = await lbresults.json();
+  console.log(lbdata);
+  const lbp = document.getElementById("lbp");
+  lbp.innerHTML = "";
+  for (let i = 0; i < lbdata.length; i++) {
+    const lbrow = document.createElement("tr");
+    const lbu = document.createElement("td");
+    lbu.textContent = lbdata[i].username;
+    lbrow.appendChild(lbu);
 
-  bestScores.push(newTime);
-  bestScores.sort((a, b) => a - b);
-  bestScores.splice(3);
+    const lbm = document.createElement("td");
+    lbm.textContent = lbdata[i].timemin + " : ";
+    lbrow.appendChild(lbm);
 
-  document.getElementById("ps1").innerText = bestScores[0];
-  document.getElementById("ps2").innerText = bestScores[1];
-  document.getElementById("ps3").innerText = bestScores[2];
-};
+    const lbs = document.createElement("td");
+    lbs.textContent = lbdata[i].timesec;
+    lbrow.appendChild(lbs);
+
+    lbp.appendChild(lbrow);
+  }
+}
+leaderboardscores();
